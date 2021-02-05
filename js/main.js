8cf2a34e2e44cc8077aa476166e4917a5d13c615
RuthlessLion <windowtheworldofgames@gmail.com>
2021-02-05 16:40:24 +0700

    Complete

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 function alertInput(){
-    var text = document.querySelector('#todoInput');
-    alert(text.value);
+    let text = document.querySelector('#todoInput');
 };
+function formAdd(form) {
+    let textInput = document.querySelector('#todoInput');
+    let todoDiv = document.querySelector('#todoDiv')
+    if (!textInput.value == ''){
+        todoDiv.innerHTML += '<div class="todoItem" onclick="todoDelElement(this)">' + textInput.value +'</div>';
+        textInput.value = '';
+    }
+}
+function todoDelElement(elem) {
+    elem.remove();
+}
